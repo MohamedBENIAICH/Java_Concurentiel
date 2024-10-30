4500a5687370578a77f050d216e069d68ed3d82c
coding-genius01 <jayasinghethilina1@gmail.com>
2024-10-30 08:59:51 +0530

    first commit

---
+++ RealTimeEventTicketingSystem/src/main/java/com/thilina_jayasinghe/w2052199/RealTimeEventTicketingSystem/RealTimeEventTicketingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.thilina_jayasinghe.w2052199.RealTimeEventTicketingSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealTimeEventTicketingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealTimeEventTicketingSystemApplication.class, args);
+	}
+
+}
